4348a6d25901f6dbc7cfeb40e2ec1caf80b29db3
mininet <Gasparila@bitbucket.com>
2015-05-18 19:45:54 -0700

    initial commit

--- run.sh
+++ run.sh
@@ -5,23 +5,10 @@
 
 time=200
 bwnet=1.5
-# TODO: If you want the RTT to be 20ms what should the delay on each
-# link be?  Set this value correctly.
 delay=10
 
 iperf_port=5001
 
-for qsize in 20 100; do
-    dir=bb-q$qsize
+dir=tfo
 
-    # TODO: Run bufferbloat.py here...
-    # XXX: difference between -B and -b for the purposes of this experiment?  I think the rest are correct.
-    python bufferbloat.py -b $bwnet --delay $delay -d $dir -t $time --maxq $qsize
-
-    # TODO: Ensure the input file names match the ones you use in
-    # bufferbloat.py script.  Also ensure the plot file names match
-    # the required naming convention when submitting your tarball.
-    python plot_tcpprobe.py -f $dir/cwnd.txt -o $dir/cwnd-iperf.png -p $iperf_port
-    python plot_queue.py -f $dir/q.txt -o $dir/q.png
-    python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
-done
+python tcp_fastopen.py -b $bwnet --delay $delay -d $dir -t $time 
--- tcp_fastopen.py
+++ tcp_fastopen.py
@@ -15,18 +15,13 @@ from time import sleep, time
 from multiprocessing import Process
 from argparse import ArgumentParser
 
-from monitor import monitor_qlen
 import termcolor as T
 
 import sys
 import os
 import math
 
-# TODO: Don't just read the TODO sections in this code.  Remember that
-# one of the goals of this assignment is for you to learn how to use
-# Mininet. :-)
-
-parser = ArgumentParser(description="Bufferbloat tests")
+parser = ArgumentParser(description="TFO tests")
 parser.add_argument('--bw-host', '-B',
                     type=float,
                     help="Bandwidth of host links (Mb/s)",
@@ -67,8 +62,8 @@ parser.add_argument('--cong',
 # Expt parameters
 args = parser.parse_args()
 
-class BBTopo(Topo):
-    "Simple topology for bufferbloat experiment."
+class Topo(Topo):
+    "Simple topology for tfo experiment."
 
     def build(self, n=2):
         # TODO: create two hosts
@@ -86,23 +81,6 @@ class BBTopo(Topo):
         self.addLink(h1, switch, bw=args.bw_host, delay=delay, max_queue_size=args.maxq)
         self.addLink(switch, h2, bw=args.bw_net, delay=delay, max_queue_size=args.maxq)
         return
-
-# Simple wrappers around monitoring utilities.  You are welcome to
-# contribute neatly written (using classes) monitoring scripts for
-# Mininet!
-def start_tcpprobe(outfile="cwnd.txt"):
-    os.system("rmmod tcp_probe; modprobe tcp_probe full=1;")
-    Popen("cat /proc/net/tcpprobe > %s/%s" % (args.dir, outfile),
-          shell=True)
-
-def stop_tcpprobe():
-    Popen("killall -9 cat", shell=True).wait()
-
-def start_qmon(iface, interval_sec=0.1, outfile="q.txt"):
-    monitor = Process(target=monitor_qlen,
-                      args=(iface, interval_sec, outfile))
-    monitor.start()
-    return monitor
 
 def start_iperf(net):
     h2 = net.get('h2')
@@ -131,24 +109,11 @@ def start_webserver(net):
     sleep(1)
     return [proc]
 
-def start_ping(net):
-    # TODO: Start a ping train from h1 to h2 (or h2 to h1, does it
-    # matter?)  Measure RTTs every 0.1 second.  Read the ping man page
-    # to see how to do this.
-
-    # Hint: Use host.popen(cmd, shell=True).  If you pass shell=True
-    # to popen, you can redirect cmd's output using shell syntax.
-    # i.e. ping ... > /path/to/ping.
-    h1, h2 = net.get('h1'), net.get('h2')
-    print("Starting ping party")
-    h1.popen('ping -i .1 {0} > {1}/ping.txt 2> {1}/ping-err.txt'.format(h2.IP(), args.dir), shell=True)
-    print('ping -i .1 {0} > {1}/ping.txt 2> {1}/ping-err.txt'.format(h2.IP(), args.dir))
-
-def bufferbloat():
+def tcp_fastopen():
     if not os.path.exists(args.dir):
         os.makedirs(args.dir)
     os.system("sysctl -w net.ipv4.tcp_congestion_control=%s" % args.cong)
-    topo = BBTopo()
+    topo = Topo()
     topo.build()
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
     net.start()
@@ -158,56 +123,14 @@ def bufferbloat():
     # This performs a basic all pairs ping test.
     net.pingAll()
 
-    # Start all the monitoring processes
-    start_tcpprobe("cwnd.txt")
 
-    # TODO: Start monitoring the queue sizes.  Since the switch I
-    # created is "s0", I monitor one of the interfaces.  Which
-    # interface?  The interface numbering starts with 1 and increases.
-    # Depending on the order you add links to your network, this
-    # number may be 1 or 2.  Ensure you use the correct number.
-    qmon = start_qmon(iface='s0-eth2',
-                      outfile='%s/q.txt' % (args.dir))
-
-    # TODO: Start iperf, webservers, etc.
     start_iperf(net)
     start_webserver(net)
-    start_ping(net)
 
-    # TODO: measure the time it takes to complete webpage transfer
-    # from h1 to h2 (say) 3 times.  Hint: check what the following
-    # command does: curl -o /dev/null -s -w %{time_total} google.com
-    # Now use the curl command to fetch webpage from the webserver you
-    # spawned on host h1 (not from google!)
-
-
-    # Hint: have a separate function to do this and you may find the
-    # loop below useful.
-    start_time = time()
-    while True:
-        # do the measurement (say) 3 times.
-        sleep(5)
-        now = time()
-        delta = now - start_time
-        if delta > args.time:
-            break
-        print "%.1fs left..." % (args.time - delta)
-
-    # TODO: compute average (and standard deviation) of the fetch
-    # times.  You don't need to plot them.  Just note it in your
-    # README and explain.
-
-    # Hint: The command below invokes a CLI which you can use to
-    # debug.  It allows you to run arbitrary commands inside your
-    # emulated hosts h1 and h2.
-    # CLI(net)
-
-    stop_tcpprobe()
-    qmon.terminate()
     net.stop()
     # Ensure that all processes you create within Mininet are killed.
     # Sometimes they require manual killing.
     Popen("pgrep -f webserver.py | xargs kill -9", shell=True).wait()
 
 if __name__ == "__main__":
-    bufferbloat()
+    tcp_fastopen()
